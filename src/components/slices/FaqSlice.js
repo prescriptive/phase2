3904bb53e4bf3ec857192c8217b586ace458bb6a
Alexander Fountain <alexanderfountain@gmail.com>
2020-12-23 17:11:38 -0600

    faq styles

--- src/components/slices/FaqSlice.js
+++ src/components/slices/FaqSlice.js
@@ -13,10 +13,35 @@ import {
 } from 'react-accessible-accordion';
 
 // Demo styles, see 'Styles' section below for some notes on use.
-import 'react-accessible-accordion/dist/fancy-example.css';
+// import 'react-accessible-accordion/dist/fancy-example.css';
 
 const FaqStyle = styled.div`
-
+.accordion__button{
+  padding:20px 0px;
+  font-size:27px;
+  font-weight:bold;
+  &:focus{
+    border:0px;
+    outline:none;
+  }
+}
+.accordion__button:before {
+    display: inline-block;
+    content: "";
+    height: 10px;
+    width: 10px;
+    margin-right: 12px;
+    border-bottom: 2px solid;
+    border-right: 2px solid;
+    -webkit-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+    top: -3px;
+    position: relative;
+}
+.accordion__button[aria-expanded=true]:before, .accordion__button[aria-selected=true]:before {
+    -webkit-transform: rotate(45deg);
+    transform: rotate(45deg);
+}
 `
 
 export const FaqSlice = ({ slice }) => {
