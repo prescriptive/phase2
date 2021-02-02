fbf57b325f0889d64f6574da95db9a86b8dfc3af
Alexander Fountain <alexander@digett.com>
2021-02-02 18:26:41 -0500

    faq font colors and padding

--- src/components/slices/FaqSlice.js
+++ src/components/slices/FaqSlice.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 import React from "react"
 import Container from "../container"
-import { RichText} from "prismic-reactjs"
+import { RichText } from "prismic-reactjs"
 import linkResolver from "../../utils/linkResolver"
 import prismicHtmlSerializer from "../../gatsby/htmlSerializer"
 import {
@@ -10,23 +10,23 @@ import {
   AccordionItemHeading,
   AccordionItemButton,
   AccordionItemPanel,
-} from 'react-accessible-accordion';
+} from "react-accessible-accordion"
 
 // Demo styles, see 'Styles' section below for some notes on use.
 // import 'react-accessible-accordion/dist/fancy-example.css';
 
 const FaqStyle = styled.div`
-.accordion__button{
-  padding:20px 0px;
-  font-size:27px;
-  font-weight:bold;
-  cursor:pointer;
-  &:focus{
-    border:0px;
-    outline:none;
+  .accordion__button {
+    padding: 20px 0px;
+    font-size: 27px;
+    font-weight: bold;
+    cursor: pointer;
+    &:focus {
+      border: 0px;
+      outline: none;
+    }
   }
-}
-.accordion__button:before {
+  .accordion__button:before {
     display: inline-block;
     content: "";
     height: 10px;
@@ -38,46 +38,37 @@ const FaqStyle = styled.div`
     transform: rotate(-45deg);
     top: -3px;
     position: relative;
-}
-.accordion__button[aria-expanded=true]:before, .accordion__button[aria-selected=true]:before {
+  }
+  .accordion__button[aria-expanded="true"]:before,
+  .accordion__button[aria-selected="true"]:before {
     -webkit-transform: rotate(45deg);
     transform: rotate(45deg);
-}
+  }
 `
 
 export const FaqSlice = ({ slice }) => {
-
-  console.log(slice)
-
   return (
     <FaqStyle>
-          <Container className="faq-slice-container">
+      <Container className="faq-slice-container">
+        <section>
           <Accordion allowMultipleExpanded allowZeroExpanded>
-          {slice.items.map((qa, index) => (
-
-
-<AccordionItem>
-<AccordionItemHeading>
-    <AccordionItemButton>
-    {qa.question.text}
-    </AccordionItemButton>
-</AccordionItemHeading>
-<AccordionItemPanel>
-<RichText
-                render={qa.answer.raw}
-                linkResolver={linkResolver}
-                htmlSerializer={prismicHtmlSerializer}
-              />
-</AccordionItemPanel>
-</AccordionItem>
-
-
-          )
-          )}
- 
-        </Accordion>
-
-          </Container>
+            {slice.items.map((qa, index) => (
+              <AccordionItem>
+                <AccordionItemHeading>
+                  <AccordionItemButton>{qa.question.text}</AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <RichText
+                    render={qa.answer.raw}
+                    linkResolver={linkResolver}
+                    htmlSerializer={prismicHtmlSerializer}
+                  />
+                </AccordionItemPanel>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </section>
+      </Container>
     </FaqStyle>
   )
 }
