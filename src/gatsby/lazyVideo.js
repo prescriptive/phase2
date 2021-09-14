714b01aa52cb5a4c1b43d70958d9c7aa393ea8cc
alexanderfountain <alexanderfountain@gmail.com>
2021-09-14 16:18:45 -0500

    loading conditional

--- src/gatsby/lazyVideo.js
+++ src/gatsby/lazyVideo.js
@@ -1,13 +1,42 @@
 import React from "react"
 import ResponsiveEmbed from "react-responsive-embed"
+class LazyVideo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loaded: false,
+    }
+  }
+  componentDidMount() {
+    window.addEventListener("load", this.handleLoad)
+  }
+  componentWillMount() {
+    window.addEventListener("load", this.handleLoad)
+  }
+  handleLoad = () => {
+    this.setState({ loaded: true })
+    console.log("loaded")
+  }
+  render() {
+    console.log(this)
 
-export const LazyVideo = ({ video_id }) => {
-  return (
-    <ResponsiveEmbed
-      className="lazyframe"
-      src={"https://www.youtube.com/embed/" + video_id}
-    />
-  )
+    return this.state.loaded ? (
+      <ResponsiveEmbed
+        className="lazyframe"
+        src={"https://www.youtube.com/embed/" + this.props.video_id}
+      />
+    ) : (
+      <h2>loading</h2>
+    )
+  }
 }
+// export const LazyVideo = ({ video_id }) => {
+//   return (
+//     <ResponsiveEmbed
+//       className="lazyframe"
+//       src={"https://www.youtube.com/embed/" + video_id}
+//     />
+//   )
+// }
 
 export default LazyVideo
